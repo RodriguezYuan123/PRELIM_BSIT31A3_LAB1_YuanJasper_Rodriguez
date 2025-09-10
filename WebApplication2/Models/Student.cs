e8e43ed5a92304e04fe886432e5914614f2effeb
YUAN JASPER RODRIGUEZ <C1949-23@itmlyceumalabang.onmicrosoft.com>
2025-09-10 17:19:33 +0800

    lab1

---
+++ WebApplication2/Models/Student.cs
@@ -0,0 +1,13 @@
+namespace WebApplication2.Models;
+
+public class Student 
+{
+    public string Name { get; set; }     
+    public string Age { get; set; }     
+    public string Course { get; set; }
+}
+
+
+
+
+
